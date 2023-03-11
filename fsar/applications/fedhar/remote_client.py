8d952bf7123b4f595accffb9438f1ff1f49d0af8
Jingwen Guo <1157835913@qq.com>
2023-03-11 21:21:45 +0800

    Delete remote_client.py

--- fsar/applications/fedhar/remote_client.py
+++
@@ -1,9 +0,0 @@
-import easyfl
-from client import FedHARClient
-from dataset import prepare_train_data, prepare_test_data, DB_NAMES
-from model import Model
-
-train_data = prepare_train_data(DB_NAMES)
-test_data = prepare_test_data(DB_NAMES)
-
-easyfl.start_remote_client(train_data=train_data, test_data=test_data, client=FedHARClient, model=Model)
